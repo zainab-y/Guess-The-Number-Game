019fb092e1c3ca25713e2188e92c0990193502cc
Zainab <zaynab@mm.st>
2019-05-04 22:55:51 +1000

    made changes to code

--- guess.py
+++ guess.py
@@ -1,5 +1,4 @@
-# This is a Guess the Number game.
-
+# This is a Guess the Number game.  
 import random
 
 guessesTaken = 0
@@ -8,7 +7,7 @@ print('Hello! What is your name? ')
 myName = input()
 
 number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and  ')
+print('Well, ' + myName + ', I am thinking of a number between 1 and  20 ')
 
 for guessesTaken in range(6):
     print('Take a guess.') # Four spaces infromt of "print"
@@ -25,9 +24,10 @@ for guessesTaken in range(6):
         break
         
     if guess == number:
-    guessesTaken = str(guessesTaken + 1)
-    print('Good job, ' + myName + '! You guess my number in ' + guessesTaken + ' guesses!')
+        guessesTaken = str(guessesTaken + 1)
+    print('Good job, ' + myName + '! You guessed my number in ' +
+          guessesTaken + ' guesses!')
 
-    if guess =! number:
+    if guess != number:
         number = str(number)
         print('Nope. The number I was thinking of was ' + number + '.')
